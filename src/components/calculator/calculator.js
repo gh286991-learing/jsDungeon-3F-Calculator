3bf59ed2d6da6272cdb43423ef61706a2c2b0490
TomJhuang <gh286991@gmail.com>
2019-09-23 22:30:13 +0800

    UI done

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -28,7 +28,10 @@ class Calculator extends React.Component {
     return (
       <div className="container">
         <div className="calculator_border">
-          <div className="screen" />
+          <div className="screen" >
+            <div className="deputy_screen" > 99+189 =789</div>
+            <div className="main_screen" > 3343432</div>
+          </div>
           <div className="button_area">
             <div className="num_area">
 
@@ -45,40 +48,65 @@ class Calculator extends React.Component {
                         className="text"
                         onClick={this.valueClick}
                         value={value}
-                      >
-                        {' '}
-                        {value}
-                        {' '}
+                      >{value}
                       </button>
                     </div>
                   );
                 })}
+
+                <div className="number_bottom">
+                  <button 
+                    type="button"
+                    className="text">
+                      0
+                  </button>
+                </div>
+                <div className="number_bottom">
+                <button 
+                    type="button"
+                    className="text">
+                      00
+                  </button>
+                </div>
+                <div className="number_bottom">
+                <button 
+                    type="button"
+                    className="text">
+                      .
+                  </button>
+                </div>
+
               </div>
+   
 
             </div>
             <div className="function_area">
-              <div className="function_bottom">
+              <button className="function_bottom">
                 <div
                   className="function_text"
-                  onClick={this.onClick}
-                >  
-                ÷
-                </div>
-              </div>
-              <div className="function_bottom">
+                  onClick={this.onClick} > ÷ </div>
+              </button>
+              <button className="function_bottom">
                 <div className="function_text"> x </div>
-              </div>
-              <div className="function_bottom">
+              </button>
+              <button className="function_bottom">
                 <div className="function_text"> + </div>
-              </div>
-              <div className="function_bottom">
+              </button>
+              <button className="function_bottom">
                 <div className="function_text"> - </div>
-              </div>
+              </button>
             </div>
-
           </div>
           <div className="bottom_button">
-                232
+              <button className="AC_button">
+                AC
+              </button>
+              <button className="AC_button">
+                ⌫
+              </button>
+              <button className="eq_button">
+                =
+              </button>
           </div>
 
         </div>
