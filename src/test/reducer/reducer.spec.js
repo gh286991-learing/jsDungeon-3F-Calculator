f651f6ca08ea815cc9f0d35bb9c8b6980b323630
TomJhuang <gh286991@gmail.com>
2019-09-28 00:15:35 +0800

    unit test add get number , #8

--- src/test/reducer/reducer.spec.js
+++ src/test/reducer/reducer.spec.js
@@ -1,39 +1,97 @@
 import expect from 'expect';
-import reducers from '../../reducers';
+import calcultor from '../../reducers';
 
-describe('Plus function test', () => {
-  it(' 0 >> 3 + 3 = 6', () => {
+describe('Clacultor function test', () => {
+  describe('Get number test', () => {
     let state;
 
-    state = reducers({
-      calcultor: {
-        num: 0,
-      },
-    }, { type: 'GET_NUM', num: '3' }); // 0 >>>> 3
+    it('Get number action undified should return inital state', () => {
+      state = calcultor({
+        calcultor: {
+          num: 10,
+        },
+      }, { type: 'undefind', num: 12 });
 
-    expect(state.calcultor.num).toBe(3);
+      const { num } = state.calcultor;
 
-    state = reducers({
-      calcultor: {
-        num: 3,
-      },
-    }, { type: 'GET_PLUS' });
+      expect(num).toBe(10);
+    });
 
-    expect(state.calcultor.operator).toBe('+'); // 3 +
+    it('Get number 0 >> 5 should return 5', () => {
+      state = calcultor({
+        calcultor: {
+          num: 0,
+        },
+      }, { type: 'GET_NUM', num: '5' });
 
-    state = reducers({
-      calcultor: {
-        num: 3,
-      },
-    }, { type: 'GET_NUM', num: '3' }); // 3 + 3
+      const { num } = state.calcultor;
+
+      expect(num).toBe(5);
+    });
 
 
-    state = reducers({
-      calcultor: {
-        num: 3, operator: '+', calcultor: 3,
-      },
-    }, { type: 'PRESS_CALCULATE' }); // 3 + 3 =
+    it('Screen number is 4 and get number 5,should return 45', () => {
+      state = calcultor({
+        calcultor: {
+          num: 4,
+        },
+      }, { type: 'GET_NUM', num: '5' });
 
-    expect(state.calcultor.num).toBe(6); // 3 + 3 = 6
+      const { num } = state.calcultor;
+
+      expect(num).toBe(45);
+
+      console.log(state);
+    });
+  });
+
+  describe('complex test for press 33 + 33 + , screen should be 66', () => {
+    let state;
+    it('Get number 0 => 3', () => {
+      state = calcultor({
+        calcultor: {
+          num: 0,
+        },
+      }, { type: 'GET_NUM', num: '3' }); // 0 >>>> 3
+
+      expect(state.calcultor.num).toBe(3);
+    });
+
+    it('Get number 3 => 33', () => {
+      state = calcultor({
+        calcultor: {
+          num: 3,
+        },
+      }, { type: 'GET_NUM', num: '3' }); // 0 >>>> 33
+
+      expect(state.calcultor.num).toBe(33);
+    });
+
+    it('Press plus , main & depty screen should show 33', () => {
+      state = calcultor({
+        calcultor: {
+          num: 33, deputy: null,
+        },
+      }, { type: 'GET_PLUS' });
+
+      const { num, deputy } = state.calcultor;
+
+      expect(num).toBe(33);
+      expect(deputy).toBe('33');
+    });
+
+    it('Press plus again, main should be 66 , depty should be 33+33', () => {
+      state = calcultor({
+        calcultor: {
+          num: 33, deputy: '33', operator: '+', calcultor: 33,
+        },
+      }, { type: 'GET_PLUS' });
+
+      const { num, deputy } = state.calcultor;
+
+      expect(num).toBe(66);
+      expect(deputy).toBe('33+33');
+    });
   });
 });
+
